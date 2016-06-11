854c11b703db4c19d261d49a6c90b0691ee807a3
David Evandro <davidevandro@gmail.com>
2016-06-11 20:25:04 -0300

    Inclusão de novo caso de teste para aumentar a cobertura de DatasetModel

--- src/test/test_model_dataset.py
+++ src/test/test_model_dataset.py
@@ -4,8 +4,8 @@
 # Author			: David Martins
 # Created			: 09/06/2016
 # Last Modified		: 11/06/2016
-# Version			: 1.0.1
-# Modifications		: 1.0.1 - Adaptação dos testes para a refatoração da classe DatasetModel
+# Version			: 1.0.2
+# Modifications		: 1.0.2 - Inclusão de novo teste para aumentar cobertura de código
 
 # Description		: Contém a classe de testes para a classe DatasetModel
 
@@ -33,7 +33,7 @@ class DatasetModelTest(ut.TestCase):
 	def test_atributos_eh_consistente_ds_cheio(self):
 		'''Verifica se o atributo *_atributos* de DatasetModel eh consistente 
 		quando *_dados* é não vazio'''
-		dados = np.zeros(shape = (500, 400))
+		dados = np.zeros(shape = (5, 4))
 		ds = dm.DatasetModel(dados)
 		#o vetor de colunas é convertido para lista para ser possível a comparação
 		self.assertEquals(pd.DataFrame(dados).columns.tolist(), ds.get_atributos())
@@ -47,9 +47,9 @@ class DatasetModelTest(ut.TestCase):
 	def test_natributos_eh_consistente_ds_cheio(self):
 		'''Verifica se o atributo *_natributos* de DatasetModel eh consistente 
 		quando *_dados* é não vazio'''
-		dados = np.zeros(shape = (500, 400))
+		dados = np.zeros(shape = (5, 4))
 		ds = dm.DatasetModel(dados)
-		self.assertEquals(400, ds.get_natributos())
+		self.assertEquals(4, ds.get_natributos())
 		
 	def test_ninstancias_eh_consistente_ds_vazio(self):
 		'''Verifica se o atributo *_ninstancias* de DatasetModel eh consistente 
@@ -60,27 +60,33 @@ class DatasetModelTest(ut.TestCase):
 	def test_ninstancias_eh_consistentes_ds_cheio(self):
 		'''Verifica se o atributo *_ninstancias* de DatasetModel eh consistente 
 		quando *_dados* é não vazio'''
-		dados = np.zeros(shape = (500, 400))
+		dados = np.zeros(shape = (5, 4))
 		ds = dm.DatasetModel(dados)
-		self.assertEquals(500, ds.get_ninstancias())
+		self.assertEquals(5, ds.get_ninstancias())
 	
 	def test_atualiza_atributos(self):
 		'''Verifica se o método atualiza_atributos atualiza os _atributos *_atributos*,
 		*_natributos* e *_ninstancias* quando *_dados* é modificado'''
 		ds = dm.DatasetModel()
-		dados = np.zeros(shape = (500, 400))
+		dados = np.zeros(shape = (5, 4))
 		ds.set_dados(dados)
 		ds.atualiza_atributos()
 		#o vetor de colunas é convertido para lista para ser possível a comparação
 		self.assertEquals(pd.DataFrame(dados).columns.tolist(), ds.get_atributos())
-		self.assertEquals(400, ds.get_natributos())
-		self.assertEquals(500, ds.get_ninstancias())
+		self.assertEquals(4, ds.get_natributos())
+		self.assertEquals(5, ds.get_ninstancias())
 		
 	
-	def test_ler_csv_carrega_dataset(self):
+	def test_ler_csv_nao_existente(self):
+		'''Verifica se o método ler_csv retorna Falso quando o recebe o 
+		nome de um arquivo csv inexiste'''
+		ds = dm.DatasetModel()
+		self.assertFalse(ds.ler_csv("blah.csv"))
+	
+	def test_ler_csv_existente_carrega_dataset(self):
 		'''Verifica se o método ler_csv le um arquivo csv existente e carrega 
 		o atributo *_dados* corretamente'''
-		dados = np.zeros(shape = (500,400))
+		dados = np.zeros(shape = (5,4))
 		df = pd.DataFrame(dados)
 		df.columns = [str(i) for i in df.columns] #os nomes dos _atributos tem que ser strings
 		nomeArquivo = "teste.csv"
@@ -92,16 +98,16 @@ class DatasetModelTest(ut.TestCase):
 		
 	def test_remover_atributo_existente(self):
 		'''Verifica se o método remover_atributo remove um atributo existente'''
-		dados = np.zeros(shape = (500,400))
+		dados = np.zeros(shape = (5,4))
 		ds = dm.DatasetModel(dados)
-		ds.remover_atributo(1)
+		ds.remover_atributos([1])
 		self.assertFalse(1 in ds.get_atributos())
 		
 	def test_remover_atributo_inexistente(self):
 		'''Verifica se o método remover_atributo avisa sobre um atributo inexistente'''
-		dados = np.zeros(shape = (500,400))
+		dados = np.zeros(shape = (5,4))
 		ds = dm.DatasetModel(dados)
-		self.assertFalse(ds.remover_atributo(800))	
+		self.assertFalse(ds.remover_atributos([8]))	
 		
 if __name__ == "__main__":
 	ut.main()
