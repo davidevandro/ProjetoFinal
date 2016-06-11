e3f078b413e4c0ec58639624184dbae46c640ab8
David Evandro <davidevandro@gmail.com>
2016-06-11 10:01:58 -0300

    Refatoração da classe DatasetModel
    - Atributos foram tornados privados
    - Implementação de getters e setters

--- src/model/datasetmodel.py
+++ src/model/datasetmodel.py
@@ -3,43 +3,42 @@
 # Script Name		: datasetmodel.py
 # Author			: David Martins
 # Created			: 10/06/2016
-# Last Modified		: 
-# Version			: 1.0.0
+# Last Modified		: 11/06/2016
+# Version			: 1.0.1
 
-# Modifications		: 
+# Modifications		: 1.0.1 - Atributos tornados privados e implementados getters e setters
 
 # Description		: Contém a classe que encapsula o dataset e permite algumas operações
 
 import pandas as pd
-import csv
 
 class DatasetModel():
 	'''Classe que encapsula o dataset e permite algumas operações
 	
 	Atributos:
 	
-	dados		: dataframe que armazena os dados do dataset
-	atributos	: lista de strings que armazena o nome dos atributos
-	natributos	: inteiro que especifica quantos atributos o dataset tem
-	ninstancias	: inteiro que especifica quantas instâncias o dataset tem
+	_dados		: dataframe que armazena os dados do dataset
+	_atributos	: lista de strings que armazena o nome dos _atributos
+	_natributos	: inteiro que especifica quantos _atributos o dataset tem
+	_ninstancias	: inteiro que especifica quantas instâncias o dataset tem
 	'''
 	
 	def __init__(self, dados = []):
 		'''Construtor da classe DatasetModel. Garante que dados seja do tipo dataframe e que os demais 
-		atributos sejam consistentes com o dataset armazenado
+		_atributos sejam consistentes com o dataset armazenado
 		
 		Atributos:
 		
-		dados	: dataframe ou matriz que armazena o dataset (default = [])
+		_dados	: dataframe ou matriz que armazena o dataset (default = [])
 		'''
-		self.dados = pd.DataFrame(dados)
+		self.set_dados(dados)
 		self.atualiza_atributos()
 		
 	def atualiza_atributos(self):
-		'''Atualiza os atributos da classe DatasetModel quando há mudança nos dados'''
-		self.atributos = self.dados.columns.tolist()
-		self.natributos = self.dados.shape[1]
-		self.ninstancias = self.dados.shape[0]
+		'''Atualiza os _atributos da classe DatasetModel quando há mudança nos dados'''
+		self._atributos = self._dados.columns.tolist()
+		self._natributos = self._dados.shape[1]
+		self._ninstancias = self._dados.shape[0]
 		
 	def ler_csv(self, nome):
 		'''Lê um arquivo csv e carrega DatasetModel com seus dados
@@ -49,7 +48,7 @@ class DatasetModel():
 		nome	: string contendo o nome do arquivo csv
 		'''
 		try:
-			self.dados = pd.DataFrame.from_csv(nome)
+			self._dados = pd.DataFrame.from_csv(nome)
 			self.atualiza_atributos()
 			return True
 		except:
@@ -62,11 +61,31 @@ class DatasetModel():
 		
 		nomeAtributo: string contendo o nome do atributo a ser removido
 		'''
-		if nomeAtributo in self.atributos:
-			self.dados.pop(nomeAtributo)
+		if nomeAtributo in self._atributos:
+			self._dados.pop(nomeAtributo)
 			self.atualiza_atributos()
 			return True
 		else:
 			return False
 		
+	def get_dados(self):
+		'''Retorna o atributo *_dados*'''
+		return self._dados
+	
+	def set_dados(self,dados):
+		'''Atribui o valor *dados* ao atributo *_dados*'''
+		self._dados = pd.DataFrame(dados)
+	
+	def get_atributos(self):
+		'''Retorna o atributo *_atributos*'''
+		return self._atributos
 		
+	def get_ninstancias(self):
+		'''Retorna o atributo *_ninstancias*'''
+		return self._ninstancias
+	
+	def get_natributos(self):
+		'''Retorna o atributo *_natributos*'''
+		return self._natributos
+	
+	
