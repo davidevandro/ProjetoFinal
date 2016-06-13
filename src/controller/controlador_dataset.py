95660ba4c721882fd6e8f532340e7872b38ca0be
David Evandro <davidevandro@gmail.com>
2016-06-13 01:44:58 -0300

    Finalização da classe ControladorDataset
    - Conexão do sinal "clicado" do botão remover
    - Método de atualização dos dados do dataset
    - Método de atualização dos dados do atributo
    - Método de atualização do status do botão remover
    - Conexão do sinal "clicado" da tabela de atributos

--- src/controller/controlador_dataset.py
+++ src/controller/controlador_dataset.py
@@ -2,7 +2,7 @@
 
 # Script Name		: controlador.py
 # Author			: David Martins
-# Created			: 10/06/2016
+# Created			: 12/06/2016
 # Last Modified		: 
 # Version			: 1.0.0
 
@@ -10,15 +10,17 @@
 
 # Description		: Contém a classe que controla a atualização das telas e adquire os dados dos modelos
 
+from PySide.QtCore import Qt
+from PySide.QtGui import QMainWindow, QFileDialog, QTableView
+
+from data.dataset import Dataset
+from model.model_dataset import DatasetModel
+from model.model_estatistica_nominal import EstatisticaNominalModel
+from model.model_estatistica_numerica import EstatisticaNumericaModel
+from view.mainGui import Ui_MainWindow
 
 
-from model import datasetmodel as dm
-import os
-
-from PySide.QtCore import *
-from PySide.QtGui import *
-
-class Controlador():
+class ControladorDataset(QMainWindow):
 	'''Classe que controla e atualiza as telas com base nos dados adquiridos das classes de modelo
 	
 	Atributos:
@@ -27,20 +29,48 @@ class Controlador():
 	_dataset		: DatasetModel com os dados carregados através da tela
 	'''
 	
-	def __init__(self, view):
+	def __init__(self, view, parent = None):
 		'''Construtor da classe Controlador. Inicia seus atributos e conecta os sinais dos botões da tela
 		a métodos
 		
-		Atributos:
+		Parâmetros:
 		
-		view		: MainGui que o controlador está controlando
+		view:	: MainGui que o controlador está controlando
 		'''
+		super(ControladorDataset, self).__init__(parent)
 		
-		#conecta controlador a tela
+		#conecta o controlador a view
 		self._view = view
-		#conecta controlador ao modelo de Dataset
-		self._dataset = dm.DatasetModel()
-				
+		self._view.setupUi(self)
+		
+		self._dataset = Dataset()	
+		
+		#conecta controlador ao modelo dos dados a ser usado
+		#pelos elementos da view
+		self._datasetmodel = DatasetModel(self._dataset)
+		
+		#conecta os sinais dos botões e das ações do usuário na tela a funções
+		self._view.abrirButton.clicked.connect(self.abrir_janela_para_escolher_arquivo)
+		self._view.tabelaAtributos.entered.connect(self.atualizar_atributo_selecionado)
+		self._view.tabelaAtributos.clicked.connect(self.atualizar_atributo_selecionado)
+		self._view.removerButton.clicked.connect(self.remover_atributos)
+		
+		#modifica parâmetros da view
+		self._view.tabelaAtributos.setSelectionBehavior(QTableView.SelectRows)
+		self._view.tabelaAtributos.setSelectionMode(QTableView.SingleSelection)
+		self._view.tabelaEstatistica.setFocusPolicy(Qt.NoFocus)
+		
+		#atribui os modelos aos elementos da tela 
+		self._view.tabelaAtributos.setModel(self._datasetmodel)
+	
+		
+	def abrir_janela_para_escolher_arquivo(self):
+		'''Abre uma janela para o usuário escolher um arquivo e chama o método abrir'''
+	
+		#o endereço do arquivo + nome estão no primeiro campo de nome
+		nome = QFileDialog.getOpenFileName(self, "Abrir", "", "Arquivos de Texto (*.csv)")[0]
+		self.abrir(nome)	
+		
 	def abrir(self, nome):
 		'''Abre o arquivo com nome *nome*  e carrega esse arquivo em DatasetModel e 
 		atualiza a aba Dados da tela
@@ -56,85 +86,88 @@ class Controlador():
 		'''
 				
 		self._view.statusbar.showMessage(u"Abrindo dataset...")
+		#avisa que o modelo vai mudar
+		
 		if self._dataset.ler_csv(nome):
-			
-			self.limpar_tabela_atributos()
-			self.carregar_dados_tabela_atributos()
-			self.atualizar_grupo_dados(nome)
-			self._view.statusbar.showMessage(u"Dataset aberto")			
-			
+
+			self._datasetmodel.beginResetModel()
+			self.atualizar_grupo_dados()
+			self._view.statusbar.showMessage(u"Dataset aberto")		
+			#avisa que o modelo encerrou a mudança
+			self._datasetmodel.endResetModel()
 			return True
 		else:
 			self._view.statusbar.showMessage(u"Nenhum dataset selecionado")
 			return False
 		
-	def limpar_tabela_atributos(self):
-		'''Limpa a *tabelaAtributos* da *_view* para poder carregar novos dados'''
-		for i in reversed(range(self._view.tabelaAtributos.rowCount())):
-				self._view.tabelaAtributos.removeRow(i)
-				
-	def carregar_dados_tabela_atributos(self):
-		'''Carrega os dados de um dataset aberto pelo usuário em *tabelaAtributos*'''
-		atributos = self._dataset.get_atributos()
-		#carrega o dataset numa tabela mostrada na aba Dados		
-		for i in range(self._dataset.get_natributos()):
-			currentRowCount = self._view.tabelaAtributos.rowCount()
-			self._view.tabelaAtributos.insertRow(currentRowCount)
-			item0 = QTableWidgetItem(str(i+1))
-			item0.setFlags(Qt.ItemIsEnabled)
-			item0.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-			self._view.tabelaAtributos.setItem(currentRowCount, 0, item0)
-			item1 = QTableWidgetItem("")
-			#para tornar o item da tabela marcável
-			item1.setFlags(Qt.ItemIsEnabled| Qt.ItemIsUserCheckable)
-			item1.setCheckState(Qt.Unchecked)
-			item1.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-			self._view.tabelaAtributos.setItem(currentRowCount,1, item1)
-			item2 = QTableWidgetItem(atributos[i])
-			item2.setFlags(Qt.ItemIsEnabled)
-			item2.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-			self._view.tabelaAtributos.setItem(currentRowCount,2, item2)
-	
-	def atualizar_grupo_dados(self, nome):
-		'''Atualiza o nome do arquivo, o número de instâncias e o número de atributos na tela
+	def atualizar_grupo_dados(self):
+		'''Atualiza os dados na tela sobre o dataset (nome, instancias e atributos)'''
 		
-		Parâmetros:
-		
-		nome: String contendo o nome completo do arquivo (nome + caminho)
-		'''
-		#pega somente o nome do arquivo sem o seu endereço
-		__, nomeArquivo = os.path.split(nome)
-			
-		self._view.nomeLabel.setText(nomeArquivo)
-		self._view.instanciaLabel.setText(str(self._dataset.get_ninstancias()))
+		self._view.nomeLabel.setText(self._dataset.get_nomearquivo())
 		self._view.atributosLabel.setText(str(self._dataset.get_natributos()))
-		
-	def atualizar_botao_remover(self, item):
-		'''Ativa/desativa o *removerButton* de acordo com a contagem de checkboxes preenchidas
-		
-		Parâmetros:
-		
-		item:	QTableWidget item marcado/clicado pelo usuário
-		'''		
+		self._view.instanciaLabel.setText(str(self._dataset.get_ninstancias()))
+	
+	def atualizar_botao_remover(self):
+		'''Ativa/desativa o *removerButton* de acordo com a contagem de checkboxes preenchidas'''	
+			
 		#se não há checkbox marcada, desativa botão de remover
-		if self.contar_checkboxes_marcadas() == 0:
+		if sum(self._dataset.get_marcados()) == 0:
 			self._view.removerButton.setEnabled(False)
 		else:
 			self._view.removerButton.setEnabled(True)
 			
-	def contar_checkboxes_marcadas(self):
-		'''Conta quantas checkboxes estão marcadas
+	def atualizar_atributo_selecionado(self, indiceClicado = None):	
+		'''Atualiza os dados do atributo na tela (nome, ausentes, distintos, tipo, estatisticas)
+		e chama atualiza_botão_remover caso uma checkbox seja marcada ou desmarcada
 		
-		Retorna:
+		Parâmetros:
 		
-		ativos:	quantidade de checkboxes marcadas'''		
-		ativos = 0
+		indiceCLicado: QModelIndex contendo a posição do item clicado
+		'''
 		
-		#loop percorre as checkboxes para contar quantas estão marcadas
-		for i in range(self._view.tabelaAtributos.rowCount()):
-			if self._view.tabelaAtributos.item(i,1).checkState() == Qt.Checked:
-				ativos += 1
+		#o método foi acionado a partir de remover dados
+		#nesse caso, deve mostrar os dados do atributo na primeira linha
+		if indiceClicado is None:
+			row = 0
+			column = 1
+		else:
+			row = indiceClicado.row()
+			column = indiceClicado.row()
 		
-		return ativos
-	
-			
+		#se o usuario marcar uma checkbox
+		if column == 0:
+			self.atualizar_botao_remover()
+		
+		#encontra o nome do atributo selecionado
+		atributo = self._dataset.get_atributos()[row]
+		#encontra a coluna do dataframe relativa ao atributo
+		dados_atributo = self._dataset.get_dados()[atributo]
+		#calcula estatisticas sobre a coluna
+		estatisticas = dados_atributo.describe()
+		#calcula o número de dados ausentes
+		ausentes = int(len(dados_atributo) - estatisticas['count'])
+		pct_ausentes = int(100 * ausentes / len(dados_atributo))
+		#coluna contém strings
+		if 'unique' in estatisticas:
+			distintos = estatisticas['unique']
+			tipo = "Nominal"
+			self._view.tabelaEstatistica.setModel(EstatisticaNominalModel(dados_atributo))
+		#coluna contém números
+		else:		
+			distintos = len(dados_atributo.unique())
+			tipo = "Numérico"
+			self._view.tabelaEstatistica.setModel(EstatisticaNumericaModel(dados_atributo))
+		
+		#atualiza os dados do atributo na tela
+		self._view.nomeAtributoLabel.setText(atributo)	
+		self._view.ausentesLabel.setText("%d (%d%%)" % (ausentes, pct_ausentes))
+		self._view.tipoLabel.setText(tipo)
+		self._view.distintosLabel.setText(str(distintos))
+		
+	def remover_atributos(self):
+		'''Remove os atributos com as checkboxes selecionadas'''
+		self._datasetmodel.beginResetModel()
+		self._dataset.remover_atributos()
+		self._datasetmodel.endResetModel()
+		self.atualizar_grupo_dados()
+		self.atualizar_atributo_selecionado()
